2eb085b98282344e894b72fda2832dba1a4a8457
Petr Stanke <petr@stanke.cz>
2025-04-04 21:50:13 +0200

    Starting creation of game over menu - first part of the tutorial

---
+++ Chess_GUI/Chess_UI/GameEndMenu.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chess_UI
+{
+    /// <summary>
+    /// Interakční logika pro GameEndMenu.xaml
+    /// </summary>
+    public partial class GameEndMenu : UserControl
+    {
+        public GameEndMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void RestartClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExitClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- Chess_GUI/Chess_UI/obj/Release/net8.0-windows/Chess_UI.AssemblyInfo.cs
+++ Chess_GUI/Chess_UI/obj/Release/net8.0-windows/Chess_UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chess_UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fe1a3daebad9e506df6023e5efae0a093b763dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d785a8f9b3fdf9bde8dc2619742a8913a02c4390")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chess_UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chess_UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
